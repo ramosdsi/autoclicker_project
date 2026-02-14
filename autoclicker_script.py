9de9cfeabf8b25dc8ad6c37123ba8e6e2d65044b
Gabriel da Silva <62047775+ramosdsi@users.noreply.github.com>
2026-02-13 23:49:10 -0300

    fix autoclicker_script.py
    fixing the function missing () and changing for keyboard.is_pressed function to stop the program.

--- autoclicker_script.py
+++ autoclicker_script.py
@@ -7,5 +7,5 @@ print('digite h para iniciar o programa')
 keyboard_lib.wait('h')
 print('digite x para encerrar o programa')
 
-while not keyboard_lib.wait('x'):
-    pyautogui.click
+while not keyboard_lib.is_pressed('x'):
+    pyautogui.click()
